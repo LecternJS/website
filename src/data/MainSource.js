512c8f46decf69c1aee393b646abf4631e0d5a6d
Jordan Jones <jordpjones@gmail.com>
2021-01-21 01:35:54 -0800

    Some changes around here.

--- src/data/MainSource.js
+++ src/data/MainSource.js
@@ -3,10 +3,10 @@ import DocsSource from './DocsSource';
 
 const branchBlacklist = new Set(['docs', 'webpack', 'v8']);
 export default new DocsSource({
-  id: 'main',
-  name: 'Main library',
-  global: 'Discord',
-  repo: 'discordjs/discord.js',
+  id: 'minecraft-protocol',
+  name: 'Minecraft Protocol',
+  global: 'Minecraft Protocol',
+  repo: 'lecternjs/minecraft-protocol',
   defaultTag: 'stable',
   branchFilter: branch => !branchBlacklist.has(branch) && !branch.startsWith('dependabot/'),
   tagFilter: tag => semver.gte(tag, '9.0.0'),
